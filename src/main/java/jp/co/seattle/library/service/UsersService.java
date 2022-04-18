62b44c19db155be6ba3a319e3a69a0fe649f9341
牛島智也 <ushijimatomoya@ushijimatomoyanoMacBook-Air.local>
2022-04-18 13:03:00 +0900

    UsersService.javaの再修正

--- src/main/java/jp/co/seattle/library/service/UsersService.java
+++ src/main/java/jp/co/seattle/library/service/UsersService.java
@@ -3,7 +3,6 @@ package jp.co.seattle.library.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 
@@ -51,7 +50,7 @@ public class UsersService {
 			UserInfo selectedUserInfo = jdbcTemplate.queryForObject(sql, new UserCountRowMapper());
 			return selectedUserInfo;
 
-		} catch (IncorrectResultSizeDataAccessException e) {
+		} catch (Exception e) {
 
 			return null;
 		}
